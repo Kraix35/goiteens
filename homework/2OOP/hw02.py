b65a4674fdc8a87b792ab83faf22cf5a0a818f1e
Max Lukichov <maxlukichov@gmail.com>
2022-02-08 20:45:45 +0200

    OOP hw02

---
+++ homework/2OOP/hw02.py
@@ -0,0 +1,51 @@
+from turtle import circle
+
+
+class Figure:
+    name = ''
+    S = float()
+
+    
+
+
+class Square(Figure):
+
+    def __init__(self, name, side):
+        self.name = name
+        self.side = int(side)
+    
+    def find_S_sq(self):
+        self.S = self.side**2
+        return f'S = {self.S}'
+
+
+    def info_sq(self):
+        return f'Figure name is {self.name}, its S is {self.S}'
+
+
+class Circle(Figure):
+
+    def __init__(self, name, r):
+        self.name = name
+        self.r = r
+
+    def find_S_crcl(self):
+        π = 3.14
+        self.S = π*(self.r**2)
+        return f'S = {self.S}'
+
+    def info_crcl(self):
+        return f'Figure name is {self.name}, its S is {self.S}'
+
+
+
+
+sq1 = Square('Square', 2)
+sq1.find_S_sq()
+print(sq1.info_sq())
+
+crcl1 = Circle('Circle', 3)
+
+print(crcl1.find_S_crcl())
+
+print(crcl1.info_crcl)
